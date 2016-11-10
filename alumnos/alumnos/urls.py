b96a1d3cc5bbc95015867f528c90e325ffc04728
PatriciaFlores <th.alien.-amoree@hotmail.com>
2016-11-10 13:56:49 -0600

    Agregando mas datos2

--- alumnos/alumnos/urls.py
+++ alumnos/alumnos/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from datos.views import hola 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^s',hola, name='hola')
 ]
